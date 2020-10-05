d56c0858eb0d8ed2f98b84e1b439b4f9e7463a45
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-10-05 16:47:49 -0700

    added clear city list feature, a local storage function that saves city names that users search, renders a city weather data on load, a few css changes, a few html changes.

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-
+$(document).ready(function(){
+    //render city list if in local storage
+    if(localStorage.getItem("cities") !== null){
+        renderCityList()   
+    };
+    //grabs the city that is active in the list
+    let cityName = $(".active")[0].innerText;
+    //api key from openweathermap.org
+    const apiKey = "&appid=27ffb5b5dc6407d635a7bcd87cd32739";
+    //city name, state code and country code divided by comma, use ISO 3166 country codes.
+    const query = "q=";
+    //openweathermap.org request url for daily
+    const urlWeather = "https://api.openweathermap.org/data/2.5/weather?";
+    //combine url, query, and api key to get a query url
+    const queryurlWeather = urlWeather + query + cityName + apiKey;
+    //this is a jQuery ajax request and promise to get the city weather data from openweathermap.org
+    $.ajax({url: queryurlWeather, method:"GET"}).then(function(response){
+        $("#city-name").text(response.name + " " + timeConverter(response.dt));
+        $("#icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
+        $("#temperature").text("Temp: " +tempKtoF(response.main.temp) + "°F");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
+        getUVIndex(apiKey, response.coord.lat, response.coord.lon);
+        getDailyForecast(apiKey, response.coord.lat, response.coord.lon);
+    });
+})
 
 $("#city-search-button").on("click", function(){
+    //grabs city name from search input bar
     const city = $("#city-search-input").val();
+    //adds city to the list
+    addCityToList(city);
+    //store city list to local storage
+    storeCityList();
     //api key
     const apiKey = "&appid=27ffb5b5dc6407d635a7bcd87cd32739";
     //city name, state code and country code divided by comma, use ISO 3166 country codes.
@@ -10,9 +40,8 @@ $("#city-search-button").on("click", function(){
     const urlWeather = "https://api.openweathermap.org/data/2.5/weather?";
     //combine url, query, and api key to get a query url
     const queryurlWeather = urlWeather + query + city + apiKey;
-
+    //this is a jQuery ajax request and promise to get the city weather data from openweathermap.org
     $.ajax({url: queryurlWeather, method:"GET"}).then(function(response){
-        console.log(response);
         $("#city-name").text(response.name + " " + timeConverter(response.dt));
         $("#icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
         $("#temperature").text("Temperature: " +tempKtoF(response.main.temp) + "°F");
@@ -35,6 +64,7 @@ function getUVIndex(apiKey, lat, lon){
     const latString = "lat=" + lat;
     const lonString = "lon=" + lon;
     const queryurl = url + latString + "&" + lonString + apiKey;
+    //this is a jQuery ajax request and promise to get the city UV Index from openweathermap.org
     $.ajax({url: queryurl, method:"GET"}).then(function(response){
         let spanEl = $("<span></span>");
         spanEl.addClass("badge p-2")
@@ -60,11 +90,11 @@ function getDailyForecast(apiKey, lat, lon) {
     const queryurlForecast = urlForecast + latString + "&" + lonString + apiKey;
 
     for(let i = 1; i < 6; i++){
+        //this is a jQuery ajax request and promise to get the city 5 day forecast data from openweathermap.org
         $.ajax({url: queryurlForecast, method:"GET"}).then(function(response){
-            console.log(response);
             $("#date-day"+i).text(timeConverter(response.daily[i].dt));
             $("#icon-day"+i).attr("src", "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png");
-            $("#temperature-day"+i).text("Temperature: " +tempKtoF(response.daily[i].temp.day) + "°F");
+            $("#temperature-day"+i).text("Temp: " +tempKtoF(response.daily[i].temp.day) + "°F");
             $("#humidity-day"+i).text("Humidity: " + response.daily[i].humidity + "%");
         });
     }
@@ -81,10 +111,71 @@ function timeConverter(UNIX_timestamp){
   };
 
 function renderNewWeatherReport(){
+    //this removes the active class from the list item
     $(".active").removeClass("active");
-    let cityName = $(this).text();
+    //this adds the active class to the selected list item
     $(this).addClass("active")
-    console.log(cityName);
+    //this grabs the selected city name
+    let cityName = $(this).text();
+    //api key from openweathermap.org
+    const apiKey = "&appid=27ffb5b5dc6407d635a7bcd87cd32739";
+    //city name, state code and country code divided by comma, use ISO 3166 country codes.
+    const query = "q=";
+    //openweathermap.org request url for daily
+    const urlWeather = "https://api.openweathermap.org/data/2.5/weather?";
+    //combine url, query, and api key to get a query url
+    const queryurlWeather = urlWeather + query + cityName + apiKey;
+    //this is a jQuery ajax request and promise to get the city weather data from openweathermap.org
+    $.ajax({url: queryurlWeather, method:"GET"}).then(function(response){
+        $("#city-name").text(response.name + " " + timeConverter(response.dt));
+        $("#icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
+        $("#temperature").text("Temp: " +tempKtoF(response.main.temp) + "°F");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
+        getUVIndex(apiKey, response.coord.lat, response.coord.lon);
+        getDailyForecast(apiKey, response.coord.lat, response.coord.lon);
+    });
 }
 
+function addCityToList(name){
+    let liEl = $("<li></li>");
+    liEl.text(name);
+    liEl.addClass("list-group-item");
+    //this removes the active class from the list item
+    $(".active").removeClass("active");
+    //this adds the active class to the selected list item
+    liEl.addClass("active")
+    $(".list-group").append(liEl);
+}
+
+function storeCityList(){
+    let cityList = [];
+    //this grabs each li element innerText which should be city names and pushes them to the cityList array
+    $("li").each(function(){
+        cityList.push($(this)[0].innerText);
+    });
+    //store cityList array to local storage
+    localStorage.setItem("cities", cityList);
+}
+
+function renderCityList(){
+    //empty out the list-group class which contains all the city names
+    $(".list-group").empty();
+    //this grabs a string of city names from local storage
+    let cityList = localStorage.getItem("cities");
+    //this stores the city names into septate indexes in cityArr
+    let cityArr = cityList.split(",");
+    //this adds each city to a new li element in the city list ul
+    cityArr.forEach(function(city){
+        addCityToList(city);
+    });
+};
+
+function clearCityList(){
+    //clears the ul element with the class list-group
+    $(".list-group").empty();
+    //clears out local storage
+    localStorage.clear();
+};
+//this adds an event listener to the whole document and listens for clicks on list-group-item element class which should be city names and renders a new weather report
 $(document).on("click", ".list-group-item", renderNewWeatherReport);
